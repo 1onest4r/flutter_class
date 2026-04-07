126db2bdd4dd6d62dd3f5027526ed9c30e2f304f
1onest4r <2024054@miu.edu.mn>
2026-04-07 14:09:01 +0800

    homework 2

--- flutter_class/lib/ui/demos/5_sqlite/sqlite_demo.dart
+++ flutter_class/lib/ui/demos/5_sqlite/sqlite_demo.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SqliteDemo extends StatelessWidget {
+class SqliteDemo extends StatefulWidget {
   const SqliteDemo({super.key});
 
+  @override
+  State<SqliteDemo> createState() => _SqliteDemoState();
+}
+
+class _SqliteDemoState extends State<SqliteDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,13 +16,13 @@ class SqliteDemo extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () {}, child: Text("insert")),
+            ElevatedButton(onPressed: () {}, child: Text('insert')),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text("query")),
+            ElevatedButton(onPressed: () {}, child: Text('query')),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text("update")),
+            ElevatedButton(onPressed: () {}, child: Text('update')),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text("delete")),
+            ElevatedButton(onPressed: () {}, child: Text('delete')),
           ],
         ),
       ),
